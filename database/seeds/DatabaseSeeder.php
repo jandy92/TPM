f925a5728dfae767fd805e3fd47cd0dad2469087
patocho <patovaldivia4@gmail.com>
2016-12-20 19:08:48 -0300

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(seeder_roles::class);
+        $this->call(seeder_permisos::class);
+
     }
 }
