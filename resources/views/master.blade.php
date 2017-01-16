4330a705266b5e4bf2c0f1961d9d0100a81d4cef
jandy92 <ale_hg13@hotmail.com>
2017-01-16 20:36:31 -0300

    jquery actualizado

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,9 +6,10 @@
 	</head>
 	<body>
 		@include('barra_navegacion')
+		<!--script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script-->
 		<script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
-		@yield('contenido')		
 		<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+		@yield('contenido')		
 		@include('layouts.widgets')
 	</body>
 </html>
