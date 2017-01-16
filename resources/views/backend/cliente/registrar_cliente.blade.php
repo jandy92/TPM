2d9e0a5db8119e66d8ba047c8a13d7a6ab215be1
jandy92 <ale_hg13@hotmail.com>
2017-01-16 20:55:58 -0300

    modal

--- resources/views/backend/cliente/registrar_cliente.blade.php
+++ resources/views/backend/cliente/registrar_cliente.blade.php
@@ -122,24 +122,26 @@
         </div>
     </div>
 </div>
-<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+
+<div class="modal fade" id="basicModal" data-keyboard="false" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&amp;times;</button>
-            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+            <h4 class="modal-title" id="myModalLabel">Administrar Contactos</h4>
             </div>
             <div class="modal-body">
                 <h3>Modal Body</h3>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <!--button type="button" class="btn btn-primary">Seleccionar</button-->
         </div>
     </div>
   </div>
 </div>
+
 <script type="text/javascript">
-	$('#modal').modal('toggle');
+	$('#basicModal').modal('toggle');
 </script>
 @endsection
