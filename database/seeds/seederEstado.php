c84b062b9ec8bc9640b5b620d0fc211084d3d90e
JaimeKlenner <jaimeklennerm@gmail.com>
2017-01-17 19:15:09 -0300

    :de:
    cambios colores

--- database/seeds/seederEstado.php
+++ database/seeds/seederEstado.php
@@ -16,7 +16,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 1,
         	'nombre'=>'Trabajo nuevo',
             'color'=>'43DBFF',
-            'color_letra'=>'9EECFE'
+            'color_letra'=>'000000'
         	));
         $e->save();
 
@@ -32,7 +32,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 3,
         	'nombre'=>'Ejecutado cot. enviada',
             'color'=>'F79646',
-            'color_letra'=>'FFC595'
+            'color_letra'=>'FFFFFF'
         	));
         $e->save();
 
@@ -40,7 +40,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 4,
         	'nombre'=>'Por pagar',
             'color'=>'92D050',
-            'color_letra'=>'CFF2A8'
+            'color_letra'=>'000000'
         	));
         $e->save();
 
@@ -55,7 +55,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 6,
         	'nombre'=>'No adjudicado',
             'color'=>'948B54',
-            'color_letra'=>'E0D9AD'
+            'color_letra'=>'FFFFFF'
         	));
         $e->save();
         
@@ -63,7 +63,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 7,
         	'nombre'=>'Por cobrar',
             'color'=>'FF0000',
-            'color_letra'=>'FF6464'
+            'color_letra'=>'FFFFFF'
         	));
         $e->save();
 
@@ -71,7 +71,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 8,
         	'nombre'=>'Ejecutado por cotizar',
             'color'=>'F79646',
-            'color_letra'=>'FFC595'
+            'color_letra'=>'FFFFFF'
         	));
         $e->save();
 
@@ -79,7 +79,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 9,
         	'nombre'=>'Nota de credito',
             'color'=>'8064A2',
-            'color_letra'=>'D0C3E0'
+            'color_letra'=>'FFFFFF'
         	));
         $e->save();
 
@@ -87,7 +87,7 @@ class seederEstado extends Seeder
         	'id_estado'=> 10,
         	'nombre'=>'En ejecución',
             'color'=>'FFFF00',
-            'color_letra'=>'9B9B00'
+            'color_letra'=>'000000'
         	));
         $e->save();
     }
