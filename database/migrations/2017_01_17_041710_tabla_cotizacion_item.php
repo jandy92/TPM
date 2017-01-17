dadc956c161ec37dd9b684e9adfc49d55348c4b0
Alejandra <ale_hg13@hotmail.com>
2017-01-17 01:21:16 -0300

    migraciones

--- database/migrations/2017_01_17_041710_tabla_cotizacion_item.php
+++ database/migrations/2017_01_17_041710_tabla_cotizacion_item.php
@@ -16,7 +16,6 @@ class TablaCotizacionItem extends Migration
        Schema::create('cotizacion_item', function (Blueprint $table){
             $table->integer('id_item')->unsigned();
             $table->integer('folio_cotizacion')->unsigned();
-            $table->softDeletes();
             
             $table->integer('cantidad');
             $table->integer('precio_unitario');
