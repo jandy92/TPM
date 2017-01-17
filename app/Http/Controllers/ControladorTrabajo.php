6c07e5d819566702a5dc2835cede85a960c28e34
JaimeKlenner <jaimeklennerm@gmail.com>
2017-01-17 02:19:10 -0300

    :de:
    comienzo de edicion trabajo

--- app/Http/Controllers/ControladorTrabajo.php
+++ app/Http/Controllers/ControladorTrabajo.php
@@ -32,6 +32,23 @@ class ControladorTrabajo extends Controller{
         	$trabajo->save();
         	$msg =['title'=>'Operacion realizada','text'=>'Se ha guardado un trabajo nuevo'];
 	    	return redirect()->action('ControladorCotizacion@listaCotizacion')->with('mensaje',$msg);
+	}
 
+	function editarTrabajoForm($id_trabajo){
+		$trabajo = Trabajo::find($id_trabajo);
+		return view('backend.trabajo.editar_trabajo', compact("trabajo"));
+	}
+	function editarTrabajo(EditarTrabajoRequest $r){
+		$trabajo->numero_factura = $r->get('numero_factura');
+		$trabajo->fecha_emision_cobro = $r->get('fecha_emision_cobro');
+		$trabajo->fecha_pago = $r->get('fecha_pago');
+		$trabajo->receptor_factura = $r->get('receptor_factura');
+		$trabajo->orden_trabajo = $r->get('orden_trabajo');
+		$trabajo->comentario = $r->get('comentario');
+		$trabajo->id_estado = $r->get('estado');
+		$trabajo->orden_compra = $r->get('orden_compra');
+		$trabajo->save();
+		$msg =['title'=>'Operacion realizada','text'=>'Se ha editado el trabajo'];
+		return redirect()->action('ControladorCotizacion@listaCotizacion')->with('mensaje',$msg);
 	}
 }
