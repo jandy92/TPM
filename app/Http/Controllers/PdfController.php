b8eae232cc277566e93b0c8eea888451834e39ec
patocho <patovaldivia4@gmail.com>
2017-01-18 20:46:10 -0300

    documentacion de controlador PDF

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -7,6 +7,27 @@ use Illuminate\Http\Request;
 
 class PdfController extends Controller
 {
+
+	/*
+	##################IMPORTANTE#############
+	Para implementar esta funcion fue necesaria la instalacion de una libreria
+	extra, la utilizada es DomPDF se puede encontrar su documentacion en su
+	github oficial: https://github.com/dompdf/dompdf
+	#########################################
+
+
+	Esta funcion es la encargada de generar un documento PDF,
+	como parametro recibe un numero de folio de una cotización, 
+	se busca en la base de datos y se asigna esa entrada a la variable $cotizacion.
+
+	A la  variable $url se le da la ubicacion de la plantilla para generar PDF en 
+	formato htlm.
+
+	$view se encarga de generar y renderizar la plantilla entregada en una vista
+	$pdf se encarga de cargar la vista entregada y crear el documento como tal, 
+	la funcion download() contiene el nombre con el cual se genera el documento
+	y lo descarga en el navegador.
+	*/
     function crearPDF($folio_cotizacion){
     	$cotizacion = Cotizacion::find($folio_cotizacion);
     	$url = ".backend.pdf.pdf_cotizacion_v2";
